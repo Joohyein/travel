1f06f3e5812d07916590e11cc439632067e1ad29
Galmaeki <kjh09800@gmail.com>
2023-01-16 11:23:00 +0900

    Merge branch 'main' of https://github.com/Joohyein/travel
    # Conflicts:
    #	app.py
    
    서버 구동부분 추가

--- app.py
+++ app.py
@@ -8,7 +8,17 @@ from pymongo import MongoClient
 client = MongoClient('mongodb+srv://test:sparta@cluster0.igj8fho.mongodb.net/cluster0?retryWrites=true&w=majority')
 db = client.dbsparta
 
-doc = {
-         'td' : "설연주"
-    }
-db.trevel.insert_one(doc)
+@app.route('/')
+def home():
+   return render_template('index.html')
+
+@app.route('/select')
+def select():
+   return render_template('select.html')
+
+@app.route('/result')
+def result():
+   return render_template('result.html')
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
